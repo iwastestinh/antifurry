393558982bcc770f3f1297e1e1b4af15aabebfab
uniquenamehere <60616380+uniquenamehere@users.noreply.github.com>
2020-02-24 15:29:07 +0100

    Update antifurry.py

--- antifurry.py
+++ antifurry.py
@@ -3,3 +3,11 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix=['af!'])
 client.remove_command('help')
+
+@client.event
+async def on_message(message):
+  if "furry" in message:
+    await message.delete
+    
+#Made by Iwtesting with love
+client.run('Token')
